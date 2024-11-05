b4769b8480264ea8d76707fc94721c7846f841f7
simplyi <sergey.kargopolov@gmail.com>
2024-11-05 15:57:56 -0500

    Reading HTTP Request Body

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
@@ -1,15 +1,17 @@
 package com.appsdeveloperblog.reactive.ws.users.presentation;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/users") //   http://localhost:8080/users
 public class UserController {
 
     @PostMapping
-    public void createUser() {
+    public void createUser(@RequestBody Mono<CreateUserRequest> createUserRequest) {
 
     }
 }
