f7e9d123228035ffa0821465b4a6fc61755c29f6
simplyi <sergey.kargopolov@gmail.com>
2024-11-08 12:37:51 -0500

    Returning UserRest object in HTTP Response Body

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/users") //   http://localhost:8080/users
 public class UserController {
 
     @PostMapping
-    public Mono<String> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
-        return Mono.just("OK");
+    public Mono<UserRest> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
+        return createUserRequest.map(request -> new UserRest(UUID.randomUUID(),
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail())
+        );
     }
 }
