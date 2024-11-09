0bfd87317b3f50be807af802f39b8f9b857da48d
simplyi <sergey.kargopolov@gmail.com>
2024-11-09 14:17:12 -0500

    @GetMapping annotation. Handle HTTP GET request to return user by userId.

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
@@ -1,12 +1,12 @@
 package com.appsdeveloperblog.reactive.ws.users.presentation;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -14,11 +14,24 @@ import java.util.UUID;
 public class UserController {
 
     @PostMapping
-    public Mono<UserRest> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
+    public Mono<ResponseEntity<UserRest>> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
         return createUserRequest.map(request -> new UserRest(UUID.randomUUID(),
                 request.getFirstName(),
                 request.getLastName(),
                 request.getEmail())
-        );
+        ).map(userRest -> ResponseEntity
+                .status(HttpStatus.CREATED)
+                .location(URI.create("/users/" + userRest.getId()))
+                .body(userRest));
+    }
+
+    @GetMapping("/{userId}")
+    public Mono<UserRest> getUser(@PathVariable("userId") UUID userId) {
+        return Mono.just(new UserRest(
+                userId,
+                "Sergey",
+                "Kargopolov",
+                "test@test.com"
+        ));
     }
 }
