f7dcf5edfa5b6854ca37c39d306d00417165dc1c
simplyi <sergey.kargopolov@gmail.com>
2024-11-12 16:03:50 -0500

    Created UserEntity class

---
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/data/UserEntity.java
@@ -0,0 +1,63 @@
+package com.appsdeveloperblog.reactive.ws.users.data;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
+
+@Table(name="users")
+public class UserEntity {
+    @Id
+    private UUID id;
+
+    @Column("first_name")
+    private String firstName;
+
+    @Column("last_name")
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
