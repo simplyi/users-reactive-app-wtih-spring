48613a601cf7e97cee8dc59c8a8cceb096df1763
simplyi <sergey.kargopolov@gmail.com>
2024-11-18 16:10:19 -0500

    Pagination. Added findAllBy(Pageable pageable) method to UserRepository interface

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepository.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepository.java
@@ -1,8 +1,11 @@
 package com.appsdeveloperblog.reactive.ws.users.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
 public interface UserRepository extends ReactiveCrudRepository<UserEntity, UUID> {
+    Flux<UserEntity> findAllBy(Pageable pageable);
 }
