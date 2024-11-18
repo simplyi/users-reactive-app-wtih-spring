1db07daffa1a1991bf15593191d4650dd64898a0
simplyi <sergey.kargopolov@gmail.com>
2024-11-18 15:06:24 -0500

    Revert "Pagination - Presentation layer"
    This reverts commit 673c490083b715039519ba940e9be70d361db7a0.

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserController.java
@@ -40,6 +40,10 @@ public class UserController {
     @GetMapping
     public Flux<UserRest> getUsers(@RequestParam(value = "offset", defaultValue = "0") int offset,
                                    @RequestParam(value = "limit", defaultValue = "50") int limit) {
-        return userService.findAll(offset, limit);
+        return Flux.just(
+                new UserRest(UUID.randomUUID(), "Sergey", "Kargopolov", "test@test.com"),
+                new UserRest(UUID.randomUUID(), "Alice", "Smith", "alice@test.com"),
+                new UserRest(UUID.randomUUID(), "Bob", "Johnson", "bob@test.com")
+        );
     }
 }
