e868746a1595eafe93ce0c221c8e356ec1c03fc9
simplyi <sergey.kargopolov@gmail.com>
2024-11-22 11:31:07 -0500

    Handle WebExchangeBindException

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/GlobalExceptionHandler.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/GlobalExceptionHandler.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +21,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Mono<ErrorResponse> handleGeneralException(Exception exception) {
         return Mono.just(ErrorResponse.builder(exception, HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public Mono<ErrorResponse> handleWebExchangeBindException(WebExchangeBindException exception) {
+        String errorMessage = exception
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error->error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return Mono.just(ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, errorMessage)
+                .build());
     }
 
 }
