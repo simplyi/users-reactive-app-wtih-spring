9b8a7667b17a536413277017e445dde5e90cd8f2
simplyi <sergey.kargopolov@gmail.com>
2024-11-27 18:48:56 -0500

    Implementing authenticate() method

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/service/AuthenticationServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/service/AuthenticationServiceImpl.java
@@ -1,14 +1,43 @@
 package com.appsdeveloperblog.reactive.ws.users.service;
 
+import com.appsdeveloperblog.reactive.ws.users.data.UserEntity;
+import com.appsdeveloperblog.reactive.ws.users.data.UserRepository;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final ReactiveAuthenticationManager reactiveAuthenticationManager;
+    private final UserRepository userRepository;
+
+    public AuthenticationServiceImpl(ReactiveAuthenticationManager reactiveAuthenticationManager,
+                                     UserRepository userRepository) {
+        this.reactiveAuthenticationManager = reactiveAuthenticationManager;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public Mono<Map<String, String>> authenticate(String username, String password) {
-        return null;
+        return reactiveAuthenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(username,password))
+                .then(getUserDetails(username))
+                .map(this::createAuthResponse);
+    }
+
+    private Mono<UserEntity> getUserDetails(String username) {
+        return userRepository.findByEmail(username);
+    }
+
+    private Map<String, String> createAuthResponse(UserEntity user) {
+        Map<String, String> result = new HashMap<>();
+        result.put("userId",user.getId().toString());
+        result.put("token","JWT"); // Replace it with actual JWT
+        return result;
     }
 }
