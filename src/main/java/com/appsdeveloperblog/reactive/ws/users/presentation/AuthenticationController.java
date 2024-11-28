972fe69bd4b239353db80a37df1546dab91614e8
simplyi <sergey.kargopolov@gmail.com>
2024-11-27 20:20:55 -0500

    Invoking authenticate() method

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/AuthenticationController.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/presentation/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.reactive.ws.users.presentation;
 
 import com.appsdeveloperblog.reactive.ws.users.presentation.model.AuthenticationRequest;
+import com.appsdeveloperblog.reactive.ws.users.service.AuthenticationService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,22 @@ import reactor.core.publisher.Mono;
 @RestController
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping("/login")
     public Mono<ResponseEntity<Void>> login(@RequestBody Mono<AuthenticationRequest> authenticationRequestMono) {
-        return Mono.just(ResponseEntity.ok().build());
+        return authenticationRequestMono
+                .flatMap(authenticationRequest ->
+                        authenticationService.authenticate(authenticationRequest.getEmail(),
+                                authenticationRequest.getPassword()))
+                .map(authenticationResultMap-> ResponseEntity.ok()
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer "
+                                + authenticationResultMap.get("token"))
+                        .header("UserId",authenticationResultMap.get("userId"))
+                        .build());
     }
 }
