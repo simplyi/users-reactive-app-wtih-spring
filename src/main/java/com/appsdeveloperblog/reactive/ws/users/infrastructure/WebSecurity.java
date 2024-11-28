3d91e367de58a3247ff364040f7b4d5c4e1e018a
simplyi <sergey.kargopolov@gmail.com>
2024-11-28 15:35:39 -0500

    Disable basic authentication and Register Reactive Authentication Manager

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/WebSecurity.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.reactive.ws.users.infrastructure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,13 +15,16 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class WebSecurity {
 
     @Bean
-    SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http) {
+    SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http,
+                                                   ReactiveAuthenticationManager authenticationManager) {
         return http
                 .authorizeExchange(exchanges->exchanges
                         .pathMatchers(HttpMethod.POST, "/users").permitAll()
                         .pathMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyExchange().authenticated())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .authenticationManager(authenticationManager)
                 .build();
     }
 
