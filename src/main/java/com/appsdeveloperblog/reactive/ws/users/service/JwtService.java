1d3e2a831705c91aca07df949b5078cf87ef69dd
simplyi <sergey.kargopolov@gmail.com>
2024-11-29 11:17:11 -0500

    Created JwtService interface and implementation class

---
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/service/JwtService.java
@@ -0,0 +1,5 @@
+package com.appsdeveloperblog.reactive.ws.users.service;
+
+public interface JwtService {
+    String generateJwt(String subject);
+}
---
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/service/JwtServiceImpl.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.reactive.ws.users.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+    @Override
+    public String generateJwt(String subject) {
+        return "";
+    }
+}
