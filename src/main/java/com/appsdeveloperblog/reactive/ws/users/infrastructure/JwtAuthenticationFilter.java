52979f7c7b7d58894ccf589f2869b61a691f8770
simplyi <sergey.kargopolov@gmail.com>
2024-11-29 18:42:50 -0500

    Created JwtAuthenticationFilter class that implements WebFilter interface

---
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/JwtAuthenticationFilter.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.reactive.ws.users.infrastructure;
+
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+public class JwtAuthenticationFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return chain.filter(exchange);
+    }
+}
