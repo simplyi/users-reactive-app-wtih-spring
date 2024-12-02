a14fee9ed0546d428b77e588cf651f537e1098bf
simplyi <sergey.kargopolov@gmail.com>
2024-12-02 09:39:35 -0500

    Extract JWT from Authorization header.

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/JwtAuthenticationFilter.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.reactive.ws.users.infrastructure;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -8,6 +10,15 @@ import reactor.core.publisher.Mono;
 public class JwtAuthenticationFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String token = extractToken(exchange);
         return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if(StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7).trim();
+        }
+        return null;
     }
 }
