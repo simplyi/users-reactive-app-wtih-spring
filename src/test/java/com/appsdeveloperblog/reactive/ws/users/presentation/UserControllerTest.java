1c62218457af3cf22c22224d30d093541bddb8de
simplyi <sergey.kargopolov@gmail.com>
2025-02-11 14:14:08 -0500

    Created a new test class and annotated it with @WebFluxTest annotation

---
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.appsdeveloperblog.reactive.ws.users.presentation;
+
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+
+@WebFluxTest(UserController.class)
+public class UserControllerTest {
+}
