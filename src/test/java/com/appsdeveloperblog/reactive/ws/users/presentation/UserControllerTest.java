7f3193a58332bcd98c454e68d5bd7611c66a1f0b
simplyi <sergey.kargopolov@gmail.com>
2025-02-11 15:29:27 -0500

    @MockitoBean annotation

--- src/test/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserControllerTest.java
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/presentation/UserControllerTest.java
@@ -1,7 +1,13 @@
 package com.appsdeveloperblog.reactive.ws.users.presentation;
 
+import com.appsdeveloperblog.reactive.ws.users.service.UserService;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @WebFluxTest(UserController.class)
 public class UserControllerTest {
+
+    @MockitoBean
+    private UserService userService;
+
 }
