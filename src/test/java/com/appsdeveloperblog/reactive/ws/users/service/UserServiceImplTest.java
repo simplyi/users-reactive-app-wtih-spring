8801c987ec3a2a9c34387b5e3e08f68be81f3244
simplyi <sergey.kargopolov@gmail.com>
2025-02-20 13:36:33 -0500

    Mocked objects in a UserServiceImplTest class

--- src/test/java/com/appsdeveloperblog/reactive/ws/users/service/UserServiceImplTest.java
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/service/UserServiceImplTest.java
@@ -1,9 +1,38 @@
 package com.appsdeveloperblog.reactive.ws.users.service;
 
+import com.appsdeveloperblog.reactive.ws.users.data.UserRepository;
+import com.appsdeveloperblog.reactive.ws.users.presentation.model.UserRest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Sinks;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private WebClient webClient;
+
+    private Sinks.Many<UserRest> usersSink;
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        usersSink = Sinks.many().multicast().onBackpressureBuffer();
+        userService = new UserServiceImpl(userRepository, passwordEncoder, usersSink, webClient);
+    }
 
     @Test
     void testCreateUser_withValidRequest_returnsCreatedUserDetails() {
