2520dd41081228cb8a5a53df267e69a44544f0fe
simplyi <sergey.kargopolov@gmail.com>
2025-02-20 13:14:46 -0500

    Created UserServiceImplTest class and an empty test method

---
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/service/UserServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.appsdeveloperblog.reactive.ws.users.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserServiceImplTest {
+
+    @Test
+    void testCreateUser_withValidRequest_returnsCreatedUserDetails() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
