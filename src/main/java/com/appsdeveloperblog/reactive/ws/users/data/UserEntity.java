2b2ab622f1e94c8e984f8a7a0343e35eac32cda2
simplyi <sergey.kargopolov@gmail.com>
2025-03-01 15:49:08 -0500

    @BeforeAll annotation and DatabaseClient

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/data/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/data/UserEntity.java
@@ -21,6 +21,17 @@ public class UserEntity {
 
     private String password;
 
+    public UserEntity() {
+    }
+
+    public UserEntity(UUID id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
     public UUID getId() {
         return id;
     }
--- src/test/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.appsdeveloperblog.reactive.ws.users.data;
 
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,8 +15,39 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserRepositoryTest {
 
+    @Autowired
+    private DatabaseClient databaseClient;
+
     @BeforeAll
     void setUp() {
+        UserEntity user1 = new UserEntity(UUID.randomUUID(),
+                "John",
+                "Doe",
+                "john.doe@example.com",
+                "123456789");
+
+        UserEntity user2 = new UserEntity(UUID.randomUUID(),
+                "Jane",
+                "Doe",
+                "jane.doe@example.com",
+                "123456789");
+
+        String insertSql = "INSERT INTO users (id, first_name, last_name, email, password) VALUES " +
+                "(:id, :firstName, :lastName, :email, :password)";
+
+        Flux.just(user1, user2)
+                .concatMap(user -> databaseClient.sql(insertSql)
+                        .bind("id", user.getId())
+                        .bind("firstName", user.getFirstName())
+                        .bind("lastName", user.getLastName())
+                        .bind("email", user.getEmail())
+                        .bind("password", user.getPassword())
+                        .fetch()
+                        .rowsUpdated())
+                .then()
+                .as(StepVerifier::create)
+                .verifyComplete();
+
     }
 
     @AfterAll
