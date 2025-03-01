3f302ff901ed55b5671c6a0f5fc41159b5d595ee
simplyi <sergey.kargopolov@gmail.com>
2025-03-01 15:06:42 -0500

    @TestInstance annotation

--- src/test/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepositoryTest.java
@@ -1,20 +1,19 @@
 package com.appsdeveloperblog.reactive.ws.users.data;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserRepositoryTest {
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
     }
 
-    @AfterEach
+    @AfterAll
     void tearDown() {
     }
 
