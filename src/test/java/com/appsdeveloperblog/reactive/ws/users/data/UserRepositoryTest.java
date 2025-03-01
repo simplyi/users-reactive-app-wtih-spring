8dc9b51b2b6a51e7eb7b8d772c41d16e009478eb
simplyi <sergey.kargopolov@gmail.com>
2025-03-01 11:05:02 -0500

    Created a new test class

---
+++ src/test/java/com/appsdeveloperblog/reactive/ws/users/data/UserRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.appsdeveloperblog.reactive.ws.users.data;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataR2dbcTest
+class UserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+}
